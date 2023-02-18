3b9099fae0ba628a2c0e86cebc8b697a0ae7583e
Tiago Epitácio <tiagoepitaciodejesus@hotmail.com>
2023-02-18 16:19:23 -0300

    Testando Cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'kwymjz',
 
-  secrets: {
-    CYPRESS_RECORD_KEY:'105a9675-a503-406b-93c8-0c8ab7fa39c6'
-  },
-
   e2e: {
 
     baseUrl: 'https://barrigarest.wcaquino.me',
